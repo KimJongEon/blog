54c31cc53c06d216c3760cb19a61f33d7b69138b
KimJongEon <kjo4134@gmail.com>
2022-09-13 14:02:16 +0900

    modal 만들어보기

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App() { // <- 이것도 컴포넌트
 
   let post = '강남 우동 맛집';
   // State 문법 : 바인딩 한 데이터를 변경 할 수 있음
@@ -73,10 +73,52 @@ function App() {
         </p>
       </div>
 
+      <Modal></Modal>
+
+      <Nav></Nav>
+      <Footer></Footer>
 
 
     </div> // App End
   ); // return() End
+}
+
+/* modal 사용 : componnet 문법 */
+// 1. function 만듦
+// 2. return() 안에 html 담기
+// 3. <함수명></함수명> 쓰기
+
+
+// 컴포넌트을 왜 사용
+// 1. 반복적인 html 축약할 때
+// 2. 큰 페이지들을 컴포넌트로 만들고 사용
+// 3. 자주 변경되는 html을 컴포넌트로 만들고 사용
+function Modal() {
+  return (
+    <>
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  ) // return EnD
+}
+
+function Nav(){
+  return(
+    <>
+      <div>Nav 입니다.</div>
+    </>
+  )
+}
+
+function Footer(){
+  return(
+    <>
+      <p>제작 : 김종언</p>
+    </>
+  )
 }
 
 export default App;
